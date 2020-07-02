01aa0f2c25d163ba9f39e0a18cd53a45d5f99e04
Cy Schubert <cy>
2020-07-01 21:30:58 -0700

    Convert to Python 3.

--- telnetenable.py
+++ telnetenable.py
@@ -41,12 +41,12 @@ def ByteSwap(data):
     a = array.array('L')
   
   if(a.itemsize != 4):
-    print "Need a type that is 4 bytes on your platform so we can fix the data!"
+    print("Need a type that is 4 bytes on your platform so we can fix the data!")
     exit(1)
 
-  a.fromstring(data)
+  a.frombytes(data)
   a.byteswap()
-  return a.tostring()
+  return a.tobytes()
 
 def GeneratePayload(mac, username, password=""):
   # Pad the input correctly
@@ -60,10 +60,10 @@ def GeneratePayload(mac, username, password=""):
   just_password = password.ljust(0x10, "\x00")
 
   cleartext = (just_mac + just_username + just_password).ljust(0x70, '\x00')
-  md5_key = MD5.new(cleartext).digest()
+  md5_key = MD5.new(cleartext.encode("ascii")).digest()
 
-  payload = ByteSwap((md5_key + cleartext).ljust(0x80, "\x00"))
-  
+  payload = ByteSwap((md5_key + cleartext.encode("ascii")).ljust(0x80, b"\x00"))
+
   secret_key = "AMBIT_TELNET_ENABLE+" + password
 
   return ByteSwap(Blowfish.new(secret_key, 1).encrypt(payload))
@@ -74,29 +74,29 @@ def SendPayload(ip, payload):
     af, socktype, proto, canonname, sa = res
     try:
       s = socket.socket(af, socktype, proto)
-    except socket.error, msg:
+    except socket.error as msg:
       s = None
       continue
 
     try:
       s.connect(sa)
-    except socket.error, msg:
+    except socket.error as msg:
       s.close()
       s= None
       continue
     break
 
   if s is None:
-    print "Could not connect to '%s:%d'" % (ip, TELNET_PORT)
+    print("Could not connect to '%s:%d'" % (ip, TELNET_PORT))
   else:
     s.send(payload)
     s.close()
-    print "Sent telnet enable payload to '%s:%d'" % (ip, TELNET_PORT)
+    print("Sent telnet enable payload to '%s:%d'" % (ip, TELNET_PORT))
   
 def main():
   args = sys.argv[1:]
   if len(args) < 3 or len(args) > 4:
-    print "usage: python telnetenable.py <ip> <mac> <username> [<password>]"
+    print("usage: python telnetenable.py <ip> <mac> <username> [<password>]")
 
   ip = args[0]
   mac = args[1]
